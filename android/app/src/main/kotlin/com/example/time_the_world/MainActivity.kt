ff9cd2ae76c24a69513ce7a2666314d1fa81044a
Lonwabo <lsibeko023b@student.wethinkcode.co.za>
2024-09-19 04:16:38 +0200

    First created widgtes

---
+++ android/app/src/main/kotlin/com/example/time_the_world/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.time_the_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:time_the_world/pages/home.dart';
+
+void main() =>
+  runApp(MaterialApp( home: Home())); //Material App
+
