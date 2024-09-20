8508c1122d441772eb083ef74f562eb937bf76a2
Lonwabo <lsibeko023b@student.wethinkcode.co.za>
2024-09-20 20:46:05 +0200

    Added a Map for routing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:time_the_world/pages/home.dart';
+import 'package:time_the_world/pages/select_location.dart';
+import 'package:time_the_world/pages/loading.dart';
+
 
 void main() =>
-  runApp(MaterialApp( home: Home())); //Material App
+  runApp(MaterialApp(
+    initialRoute: '/home',
+     routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => SelectLocation(),
+
+     },
+     
+     
+     )); //Material App
 
