5a3ad194b133e58a53da864d4b2293b684af2f0c
Lonwabo <lsibeko023b@student.wethinkcode.co.za>
2024-09-20 20:50:16 +0200

    Added a background colour for the widget and the AppBar.

--- lib/pages/select_location.dart
+++ lib/pages/select_location.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 
 class SelectLocation extends StatefulWidget {
+  const SelectLocation({super.key});
+
 
   @override
   _SelectLocationState createState() => _SelectLocationState();
@@ -11,6 +13,13 @@ class _SelectLocationState extends State<SelectLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 75, 74, 74),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 4, 55, 131),
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
       body: Text('select location screen'),
     );
   }
