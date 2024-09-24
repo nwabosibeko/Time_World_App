473296120fe9027cf56ae273bb005e57c75e1de5
Lonwabo <lsibeko023b@student.wethinkcode.co.za>
2024-09-24 23:28:22 +0200

    Created a new package Utility, world_time.dart, added getTime (reusability).

---
+++ lib/utility/world_time.dart
@@ -0,0 +1,38 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+
+  String location = ""; //loaction name for UI
+  String time = ""; //time in location
+
+  String flag = ""; // url to asset flag icon
+  String url = "";// location url for API endpoint
+
+
+  void getTime() async{
+
+     try{
+         Response response = await get(Uri.parse("http://worldtimeapi.org/api/timezone/$url"));
+         if (response.statusCode == 200){
+
+          Map timeData = jsonDecode(response.body);
+
+          String timeDate = timeData['datetime'], offset = timeData['utc_offset'].toString().substring(1,3);// print data
+
+
+          DateTime present = DateTime.parse(timeDate);// convert it into a date time objecct.
+          present = present.add(Duration(hours: int.parse(offset)));
+          
+         }
+         else{print('Response has failed due to this code: {$response.statusCode}');}
+     }
+     catch (e){
+        print("ERROR: $e");
+     }
+
+
+  }
+
+
+}
