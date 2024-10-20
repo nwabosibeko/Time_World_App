76d5669156149b1c50a7d751327a83559f8b19af
nwabosibeko <nwabosibeko17@gmail.com>
2024-10-20 16:28:45 +0200

    design changes

--- lib/pages/select_location.dart
+++ lib/pages/select_location.dart
@@ -1,101 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:time_the_world/utility/world_time.dart';
 
-
 class SelectLocation extends StatefulWidget {
   const SelectLocation({super.key});
-
 
   @override
   _SelectLocationState createState() => _SelectLocationState();
 }
 
 class _SelectLocationState extends State<SelectLocation> {
-
-  List<WorldTime> locate = [WorldTime(url: 'Africa/Tunis', location: 'Tunis', flag: 'Tunis.png'), 
-                            WorldTime(url: 'Africa/Nigeria', location: 'Nigeria', flag: 'Nigeria.webp'),
-                            WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'nairobi.webp'),
-                            WorldTime(url: 'Africa/Windhoek', location: 'Windhoek', flag: 'windoek.jpg'),
-                            WorldTime(url: 'America/Detroit', location: 'Detroit', flag: 'detroit.gif'),
-                            WorldTime(url: 'America/Los_Angeles', location: 'Los_Angeles', flag: 'Los Angeles.png'),
-                            WorldTime(url: 'America/Mexico_City', location: 'Mexico_City', flag: 'Mexico.webp'),
-                            WorldTime(url: 'Antarctica/Macquarie', location: 'Macquarie', flag: 'antarctica.webp'),
-                            WorldTime(url: 'Antarctica/Palmer', location: 'Palmer', flag: 'palmer.jpg'),
-                            WorldTime(url: 'Asia/Bangkok', location: 'Bangkok', flag: 'Bangkok.png'),
-                            WorldTime(url: 'Europe/London', location: 'London', flag: 'london.jpg'),
-                            WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'Berlin.png'),
-                            WorldTime(url: 'Indian/Mauritius', location: 'Mauritius', flag: 'Mauritius.jpg'),
-                            WorldTime(url: 'Europe/Paris', location: 'Paris', flag: 'Paris.jpg'),
-                            WorldTime(url: 'Pacific/Tahiti', location: 'Tahiti', flag: 'Tahiti.png'),
-                            WorldTime(url: 'Africa/Johannesburg', location: 'Johannesburg', flag: 'South Africa.jpg')];
+  List<WorldTime> locate = [
+    WorldTime(url: 'Africa/Tunis', location: 'Tunis', flag: 'Tunis.png'),
+    WorldTime(url: 'Africa/Nigeria', location: 'Nigeria', flag: 'Nigeria.webp'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'nairobi.webp'),
+    WorldTime(url: 'Africa/Windhoek', location: 'Windhoek', flag: 'windoek.jpg'),
+    WorldTime(url: 'America/Detroit', location: 'Detroit', flag: 'detroit.gif'),
+    WorldTime(url: 'America/Los_Angeles', location: 'Los_Angeles', flag: 'Los Angeles.png'),
+    WorldTime(url: 'America/Mexico_City', location: 'Mexico_City', flag: 'Mexico.webp'),
+    WorldTime(url: 'Antarctica/Macquarie', location: 'Macquarie', flag: 'antarctica.webp'),
+    WorldTime(url: 'Antarctica/Palmer', location: 'Palmer', flag: 'palmer.jpg'),
+    WorldTime(url: 'Asia/Bangkok', location: 'Bangkok', flag: 'Bangkok.png'),
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'london.jpg'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'Berlin.png'),
+    WorldTime(url: 'Indian/Mauritius', location: 'Mauritius', flag: 'Mauritius.jpg'),
+    WorldTime(url: 'Europe/Paris', location: 'Paris', flag: 'Paris.jpg'),
+    WorldTime(url: 'Pacific/Tahiti', location: 'Tahiti', flag: 'Tahiti.png'),
+    WorldTime(url: 'Africa/Johannesburg', location: 'Johannesburg', flag: 'South Africa.jpg')
+  ];
 
   void changeTime(index) async {
-
     WorldTime ins = locate[index];
     await ins.getTime();
     // go to home screen
     Navigator.pop(context, {
-              'location': ins.location, 'flag': ins.flag, 'time': ins.time, 'isDay': ins.dayTime,
-
+      'location': ins.location,
+      'flag': ins.flag,
+      'time': ins.time,
+      'isDay': ins.dayTime,
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
-    // print('build function ran');
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 75, 74, 74),
+      backgroundColor: Color.fromARGB(255, 197, 196, 196),
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 4, 55, 131),
-        title: Text('Choose a Location'),
+        backgroundColor: Color.fromARGB(255, 15, 15, 15),
+        title: Text(
+          'Choose a Location',
+          style: TextStyle(color: Colors.white), 
+        ),
         centerTitle: true,
         elevation: 0,
       ),
       body: ListView.builder(
         itemCount: locate.length,
-        itemBuilder: (context, index){
+        itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
             child: Card(
+              color: Colors.grey[850], 
               child: ListTile(
                 onTap: () {
                   changeTime(index);
                 },
-                title: Text(locate[index].location),
+                title: Text(
+                  locate[index].location,
+                  style: TextStyle(color: Colors.white), 
+                ),
                 leading: CircleAvatar(
                   backgroundImage: AssetImage('resources/${locate[index].flag}'),
                 ),
               ),
             ),
           );
-        }
+        },
       ),
     );
   }
 }
-
-  // void getData() async{
-
-  //   //network simulation request for username
-  //   String name = await Future.delayed(Duration(seconds: 3), () {
-  //       return 'Nwabo';
-
-  //   });
-
-  //    //network simulation request for username
-  //   String about = await Future.delayed(Duration(seconds: 2), () {
-  //       return 'meat, alcohol, water, and Software Engineer';
-
-  //   });
-
-  //   print("The statement");
-  //   print('$name and $about');
-  // }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   getData();
-  //   print('Hey There!!!');
-  // }
